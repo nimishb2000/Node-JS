74be5fc031c48e17748e79a3347b8a17a0364c61
nimishb2000 <nimishb2000@gmail.com>
2019-09-02 22:44:27 +0530

    Redirecting response

--- Server.js
+++ Server.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         res.write('<html><head><title>Message</title></head><body><form action="/message" method="POST"><textarea rows=5 cols=10></textarea><button type="submit">Send</button></form></body></html>')
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('msg.txt', 'Dummy Text');
+        res.statusCode = 302;
+        res.setHeader('location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
