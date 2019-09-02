7008de9197e7bf5a9305d66643c0aaf8d4056314
nimishb2000 <nimishb2000@gmail.com>
2019-09-02 22:33:18 +0530

    Handling Requests

--- Server.js
+++ Server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    //console.log(req);
-    //process.exit();
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html><head><title>Message</title></head><body><form action="/message" method="POST"><textarea rows=5 cols=10></textarea><button type="submit">Send</button></form></body></html>')
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Hi</title></head>');
