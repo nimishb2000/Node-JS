5925bde36007f4cab10550f62507891abc6e1c05
nimishb2000 <nimishb2000@gmail.com>
2019-09-06 15:37:36 +0530

    Using express

---
+++ Routes/Admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const dirName = require('../util/path');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(dirName, 'Views', 'add-product.html'));
+});
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
--- Server.js
+++ Server.js
@@ -1,23 +1,18 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html><head><title>Message</title></head><body><form action="/message" method="POST"><textarea rows=5 cols=10></textarea><button type="submit">Send</button></form></body></html>')
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('msg.txt', 'Dummy Text');
-        res.statusCode = 302;
-        res.setHeader('location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hi</title></head>');
-    res.write('<body><h1>Hello World</h1></body>');
-    res.write('</html>');
-    res.end();
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./Routes/Admin');
+const shopRoutes = require('./Routes/Shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'Views', '404.html'));
 });
-server.listen(3000);
+
+app.listen(3000);
